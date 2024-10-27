c47cee2013e9857891025b3acc530c3c36f95832
Unumberten <honestgceba@gmail.com>
2024-10-27 13:12:37 +0200

    Update main.py

--- python/backend/main.py
+++ python/backend/main.py
@@ -106,4 +106,4 @@ async def sendMail(request: Request, Name:str=Form(...), Email:str=Form(...), Me
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=${PORT} )
